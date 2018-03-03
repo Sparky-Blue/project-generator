31d6f36ff49e6f79de35bee76354a5e148cc3761
Clare Pickering <clarelpickering@hotmail.com>
2018-03-03 13:36:43 +0000

    refactor generator using promises

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
 const {
-  eslintData,
-  jsonData,
-  gitIgnoreData,
-  cssNormalizeData
+  filesHTML,
+  filesNPM,
+  filesServer,
+  HTMLdirs,
+  NPMDirs,
+  serverDirs
 } = require("./data");
+const {
+  createDirectories,
+  createFiles,
+  inits,
+  gitRemoteSetup
+} = require("./utils/utils");
 const inquirer = require("inquirer");
-const child_process = require("child_process");
 
 const projectTypes = ["NPM_Project", "HTML_Project"];
 
@@ -52,138 +58,30 @@ inquirer.prompt(QUESTIONS).then(answers => {
 });
 
 function generator(projectChoice, name = "new_project", gitRemote) {
-  let arrayOfFiles = [];
-  if (projectChoice === "NPM_Project")
-    arrayOfFiles = [
-      { name: "lint", path: `./${name}/.eslintrc`, data: eslintData },
-      { name: "gitIgnore", path: `./${name}/.gitignore`, data: gitIgnoreData },
-      { name: "pJSON", path: `./${name}/package.json`, data: jsonData },
-      { name: "ReadMe", path: `./${name}/README.md`, data: "Read me..." },
-      {
-        name: "indexFile",
-        path: `./${name}/index.js`,
-        data: "module.exports = {};"
-      }
-    ];
-  else if (projectChoice === "HTML_Project")
-    arrayOfFiles = [
-      { name: "lint", path: `./${name}/.eslintrc`, data: eslintData },
-      { name: "gitIgnore", path: `./${name}/.gitignore`, data: gitIgnoreData },
-      { name: "ReadMe", path: `./${name}/README.md`, data: "Read me..." },
-      {
-        name: "index.html",
-        path: `./${name}/index.html`,
-        data: " "
-      },
-      { name: "css", path: `./${name}/css/main.css`, data: " " },
-      {
-        name: "normCSS",
-        path: `./${name}/css/normalize.css`,
-        data: cssNormalizeData
-      },
-      { name: "index.js", path: `./${name}/js/index.js`, data: " " }
-    ];
-  makeDir(name, gitRemote, arrayOfFiles, projectChoice, createProjectFolder);
-}
-
-function makeDir(path, gitRemote, array, projectChoice, cb) {
-  fs.mkdir(path, err => {
-    if (err) console.log("makeDir error" + err + path);
-    cb(null, path, gitRemote, array, projectChoice);
-  });
-}
-
-function createProjectFolder(err, data, gitRemote, array, projectChoice) {
-  let fileCount = 0;
-  if (err) console.log("Create ProjectFolder error: " + err);
-  else {
-    if (projectChoice === "NPM_Project") {
-      makeDir(`${data}/spec`, gitRemote, array, null, (err, data) => {
-        if (err) console.log(err);
-        else {
-          makeFile(
-            `./${data}/index.spec.js`,
-            "const {expect} = require('chai');",
-            callBack
-          );
-        }
-      });
-    } else if (projectChoice === "HTML_Project") {
-      makeDir(`${data}/css`, gitRemote, array, null, err => {
-        if (err) console.log(err);
-        else {
-          console.log("complete css");
-        }
-      });
-      makeDir(`${data}/images`, gitRemote, array, null, err => {
-        if (err) console.log(err);
-        else {
-          console.log("complete images/");
-        }
-      });
-      makeDir(`${data}/js`, gitRemote, array, null, err => {
-        if (err) console.log(err);
-        else {
-          console.log("complete js/");
-        }
-      });
-    }
-    array.forEach(file => {
-      makeFile(file.path, file.data, callBack);
-      fileCount++;
-      if (fileCount === array.length) {
-        if (projectChoice === "NPM_Project") {
-          inits(null, data, gitRemote, gitRemoteSetup);
-        } else if (gitRemote) {
-          gitRemoteSetup(data, gitRemote);
-        }
-      }
-    });
+  let filesList = [];
+  let directoryList = [];
+  if (projectChoice === "NPM_Project") {
+    filesList = filesNPM;
+    directoryList = NPMDirs;
+  } else if (projectChoice === "HTML_Project") {
+    filesList = filesHTML;
+    directoryList = HTMLdirs;
+  } else if (projectChoice === "Server_Project") {
+    filesList = filesServer;
+    directoryList = serverDirs;
   }
+  createDirectories(name)
+    .then(() => {
+      return createDirectories(name, directoryList);
+    })
+    .then(() => {
+      return createFiles(name, filesList);
+    })
+    .then(() => {
+      if (projectChoice === "HTML_Project") return inits(name);
+    })
+    .then(() => {
+      if (gitRemote) return gitRemoteSetup(name, gitRemote);
+    })
+    .catch(err => console.log(err));
 }
-
-function makeFile(name, data, cb) {
-  fs.writeFile(name, data, "utf8", err => {
-    if (err) console.log("writeFile error" + err);
-    else cb(null, name);
-  });
-}
-
-function inits(err, name, gitRemote, cb) {
-  if (err) console.log(err);
-  else {
-    child_process.exec(
-      "npm init -y && npm i chai mocha -D",
-      { cwd: `./${name}` },
-      err => {
-        if (err) console.log(err);
-        else {
-          console.log("npm init complete");
-        }
-      }
-    );
-    if (gitRemote) cb(name, gitRemote);
-  }
-}
-
-function gitRemoteSetup(name, gitRemote) {
-  child_process.exec(
-    `git init && git remote add origin ${gitRemote} && git add . && git commit -m 'initial project setup' && git push origin master`,
-    { cwd: `./${name}` },
-    err => {
-      if (err) console.log(err);
-      else {
-        console.log(
-          `git remote added: ${gitRemote} and setup pushed to origin master`
-        );
-      }
-    }
-  );
-}
-
-function callBack(err, data) {
-  if (err) console.log("error: " + err);
-  console.log(`created : ${data}`);
-}
-
-module.exports = { makeDir, createProjectFolder };
---
+++ utils/utils.js
@@ -0,0 +1,35 @@
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const CURR_DIR = process.cwd();
+const child_process = require("child_process");
+
+const path = require("path");
+
+function createDirectories(name, directoryList = [""]) {
+  const directoryCreate = directoryList.map(directory => {
+    return fs.mkdirAsync(`${name}/${directory}`);
+  });
+  return Promise.all(directoryCreate);
+}
+
+function createFiles(name, fileList) {
+  const fileCreate = fileList.map(file => {
+    return fs.writeFileAsync(`${CURR_DIR}/${name}${file.path}`, file.data);
+  });
+  return Promise.all(fileCreate);
+}
+
+function inits(name) {
+  child_process.exec("npm init -y && npm i chai mocha -D", {
+    cwd: `./${name}`
+  });
+}
+
+function gitRemoteSetup(name, gitRemote) {
+  child_process.exec(
+    `git init && git remote add origin ${gitRemote} && git add . && git commit -m 'initial project setup' && git push origin master`,
+    { cwd: `./${name}` }
+  );
+}
+
+module.exports = { createDirectories, createFiles, inits, gitRemoteSetup };
